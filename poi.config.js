c2e9e4e337990ad30a2a7e4b9481983bbb9f1c87
Jacek Pietal <jacek@gamecodehq.com>
2019-06-25 21:41:51 +0200

    more updats to gfx / rwd

--- poi.config.js
+++ poi.config.js
@@ -3,20 +3,22 @@ const repoLatestCommit = require('repo-latest-commit')
 const pkg = require('./package')
 
 const cdns = {
-  BABEL_CDN: 'https://cdn.jsdelivr.net/npm/@babel/standalone@7.0.0-beta.32/babel.min.js',
+  BABEL_CDN:
+    'https://cdn.jsdelivr.net/npm/@babel/standalone@7.0.0-beta.32/babel.min.js',
   PUG_CDN: 'https://cdn.jsdelivr.net/npm/browserified-pug@0.3.0/index.js',
-  CSSNEXT_CDN: 'https://cdn.jsdelivr.net/npm/browserified-postcss-cssnext@0.3.0/index.js',
-  POSTCSS_CDN: 'https://cdn.jsdelivr.net/npm/browserified-postcss@0.3.0/index.js',
-  TYPESCRIPT_CDN: 'https://cdn.jsdelivr.net/npm/browserified-typescript@0.3.0/index.js'
+  CSSNEXT_CDN:
+    'https://cdn.jsdelivr.net/npm/browserified-postcss-cssnext@0.3.0/index.js',
+  POSTCSS_CDN:
+    'https://cdn.jsdelivr.net/npm/browserified-postcss@0.3.0/index.js',
+  TYPESCRIPT_CDN:
+    'https://cdn.jsdelivr.net/npm/browserified-typescript@0.3.0/index.js'
 }
 
 module.exports = {
   extendWebpack(config) {
     config.module.set('noParse', /babel-preset-vue/)
 
-    config.module.rule('js')
-      .include
-      .add(nodeModules())
+    config.module.rule('js').include.add(nodeModules())
 
     config.node.set('fs', 'empty')
 
@@ -29,10 +31,13 @@ module.exports = {
   },
   hash: false,
   homepage: '/',
-  env: Object.assign({
-    VERSION: `v${pkg.version}-${repoLatestCommit().commit.slice(0, 7)}`,
-    LATEST_COMMIT: repoLatestCommit().commit.slice(0, 7)
-  }, cdns),
+  env: Object.assign(
+    {
+      VERSION: `v${pkg.version}-${repoLatestCommit().commit.slice(0, 7)}`,
+      LATEST_COMMIT: repoLatestCommit().commit.slice(0, 7)
+    },
+    cdns
+  ),
   presets: [
     require('poi-preset-bundle-report')(),
     require('poi-preset-babel-minify')(),
@@ -54,22 +59,28 @@ module.exports = {
           events: true,
           FALLBACK: { '/': '/' }
         },
-        externals: [].concat(Object.keys(cdns).reduce((res, name) => {
-          return res.concat(cdns[name])
-        }, []))
+        externals: [].concat(
+          Object.keys(cdns).reduce((res, name) => {
+            return res.concat(cdns[name])
+          }, [])
+        )
       }
     })
   ],
   babel: {
     babelrc: false,
-    presets: [
-      require.resolve('babel-preset-poi')
-    ],
-    plugins: [[require.resolve('babel-plugin-component'), [
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
-    ]]]
+    presets: [require.resolve('babel-preset-poi')],
+    plugins: [
+      // require.resolve('@poi/plugin-vue-static'),
+      [
+        require.resolve('babel-plugin-component'),
+        [
+          {
+            libraryName: 'element-ui',
+            styleLibraryName: 'theme-chalk'
+          }
+        ]
+      ]
+    ]
   }
 }
